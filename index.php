77106a0a08cf1818c93679a7ff35bd5a77acc9f5
Martijn Engler <mail@martijnengler.nl>
2019-10-05 13:39:27 +0200

    Move sending download headers to own function

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <?php
 //Written in 25 August 2017 By SaeedEY.com
 
-function disable_zlib_compression()
+function download_headers()
 {
+	// I'm not sure why, but we can't use zlib
 	if(ini_get('zlib.output_compression'))
 	{
 		ini_set('zlib.output_compression', 'Off');
+	}
+
+	// https://stackoverflow.com/a/12388254
+	header("Pragma: public"); // required
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+	header("Cache-Control: private",false); // required for certain browsers
+
+	$mime_type = "image/jpeg";
+	$name = "image.jpg";
+
+
+	header('Content-Type: ' . $mime_type);
+	header('Content-Disposition: attachment; filename="'.$name.'"');
+	header("Content-Transfer-Encoding: binary");
+	header('Accept-Ranges: bytes');
+
+	// set up headers for range request or just content length
+	// both development and production are currently *not* using
+	// http_range
+	if(isset($_SERVER['HTTP_RANGE']))
+	{
+		$range = get_range();
+	}
+	else
+	{
+		$new_length=$size;
+		header("Content-Length: " . $size);
 	}
 }
 
@@ -23,38 +52,7 @@ function takeScreenShot($link){
 				}
 				else
 				{
-					// I'm not sure why, but we can't use zlib
-					disable_zlib_compression();
-
-					// https://stackoverflow.com/a/12388254
-					header("Pragma: public"); // required
-					header("Expires: 0");
-					header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-					header("Cache-Control: private",false); // required for certain browsers
-
-					$mime_type = "image/jpeg";
-					$name = "image.jpg";
-
-
-					header('Content-Type: ' . $mime_type);
-					header('Content-Disposition: attachment; filename="'.$name.'"');
-					header("Content-Transfer-Encoding: binary");
-					header('Accept-Ranges: bytes');
-
-					// set up headers for range request or just content length
-					// both development and production are currently *not* using
-					// http_range
-					if(isset($_SERVER['HTTP_RANGE']))
-					{
-						$range = get_range();
-					}
-					else
-					{
-						$new_length=$size;
-						header("Content-Length: " . $size);
-					}
-
-
+					download_headers();
 				}
 					print base64_decode($screenshot);
         //exit();
