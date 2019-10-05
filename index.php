43ee2224f86249d9affd9132573d732932a84510
Martijn Engler <mail@martijnengler.nl>
2019-10-05 13:36:50 +0200

    Add option (via query string) to download image

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 //Written in 25 August 2017 By SaeedEY.com
+
+function disable_zlib_compression()
+{
+	if(ini_get('zlib.output_compression'))
+	{
+		ini_set('zlib.output_compression', 'Off');
+	}
+}
+
 function takeScreenShot($link){
     //website url
     $siteURL = $link;
@@ -8,8 +17,46 @@ function takeScreenShot($link){
         $googlePagespeedData = json_decode($googlePagespeedData, true);
         $screenshot = $googlePagespeedData['screenshot']['data'];
         $screenshot = str_replace(array('_','-'),array('/','+'),$screenshot);
-        header('Content-type: image/jpeg');
-        print base64_decode($screenshot);
+				if(empty($_GET['download']) and false)
+				{
+					header('Content-type: image/jpeg');
+				}
+				else
+				{
+					// I'm not sure why, but we can't use zlib
+					disable_zlib_compression();
+
+					// https://stackoverflow.com/a/12388254
+					header("Pragma: public"); // required
+					header("Expires: 0");
+					header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+					header("Cache-Control: private",false); // required for certain browsers
+
+					$mime_type = "image/jpeg";
+					$name = "image.jpg";
+
+
+					header('Content-Type: ' . $mime_type);
+					header('Content-Disposition: attachment; filename="'.$name.'"');
+					header("Content-Transfer-Encoding: binary");
+					header('Accept-Ranges: bytes');
+
+					// set up headers for range request or just content length
+					// both development and production are currently *not* using
+					// http_range
+					if(isset($_SERVER['HTTP_RANGE']))
+					{
+						$range = get_range();
+					}
+					else
+					{
+						$new_length=$size;
+						header("Content-Length: " . $size);
+					}
+
+
+				}
+					print base64_decode($screenshot);
         //exit();
     }
 }
@@ -17,3 +64,4 @@ $url = isset($_GET['link'])?$_GET['link']:false;
 if($url !== false)
     takeScreenShot($url);
 ?>
+
